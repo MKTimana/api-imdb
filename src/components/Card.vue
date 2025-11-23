3df2b8573f86c5cff8949802fd7afe08db18681a
Hamzah Braimo <hamzah.brm@gmail.com>
2025-11-24 01:43:28 +0200

    merge with FEAT/searchbar

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,22 +1,36 @@
-<script setup>
-defineProps({
-  movie: {
-    type: Object,
-    required: true,
-  },
-});
+<script>
+import {ref} from "vue";
 
+export default {
+  props:{
+    movie: {
+      type: Object,
+      required: true,
+    },
+  },
+  data(){
+    return{
+      extend: false
+    }
+  },
+  methods:{
+    handleExtend(){
+      this.extend = !this.extend;
+    }
+  }
+}
 </script>
 
 <template>
   <div
-    class="w-full md:w-2/3 lg:w-full dark:bg-dark-200/30 flex gap-3 text-dark-300 dark:text-surface-100 rounded-xl p-4 shadow-md shadow-black/50"
+    class="relative w-full md:w-2/3 lg:w-full dark:bg-dark-200/30 flex flex-col text-dark-300 dark:text-surface-100 rounded-xl p-4 shadow-md shadow-black/50"
   >
+  <div class="flex gap-3 ">
     <img
-      :src="`https://api.themoviedb.org/3${movie.poster_path}`"
-      :alt="movie.title"
+    :src="`https://api.themoviedb.org/3${movie.poster_path}`"
+    :alt="movie.title"
       class="flex min-w-32 max-w-40 min-h-44 max-h-56 border shrink-0 text-sm"
-    />
+      />
     <span class="flex flex-col gap-1 sm:gap-2.5 justify-evenly">
       <h2 class="text-xl font-semibold w-full border-b-2 border-accent-300">
         {{ movie.title }}
@@ -34,13 +48,24 @@ defineProps({
         Rating: {{ (movie.vote_average) }}
       </li>
     </ul>
+    <span class="flex gap-3 items-center">
       <button
-        class="outline-0 transition-smooth self-center w-full rounded-lg shadow-sm shadow-black/30 cursor-pointer hover:scale-95 px-2 py-0.5 bg-linear-to-t from-accent-300 to-accent-200 sm:max-w-1/2"
+      class="outline-0 transition-smooth self-center w-full rounded-lg shadow-sm shadow-black/30 cursor-pointer hover:scale-95 px-2 py-0.5 bg-linear-to-t from-accent-300 to-accent-200 sm:max-w-1/2"
       >
-        View
-      </button>
-    </span>
-  </div>
+      View
+    </button>
+    <button v-if="movie.overview" @click="handleExtend" class="px-2 rounded-full bg-surface-300 dark:bg-dark-100" >
+      overview
+    </button>
+  </span>
+</span>
+</div>
+
+<!-- Details  -->
+<p class="text-xs w-full text-ellipsis line-clamp-6 text-justify transition-smooth" :class="!extend ? 'h-0' : 'p-3 h-32 sm:h-16'">
+  {{ movie.overview }}
+</p>
+</div>
 </template>
 
 <style scoped></style>
