a44e8101211151271d42de49d381c453393e26e5
Hamzah Braimo <hamzah.brm@gmail.com>
2025-11-23 23:43:54 +0200

    Added a search bar (search input + search button)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,33 @@
+<script>
+import Card from './components/Card.vue'
+import Searchbar from './components/Searchbar.vue'
+export default {
+  components: {
+    Card, Searchbar
+  },
+  data() {
+    return {
+      movies: [
+        {title: "Home", image_url: "https://via.placeholder.com/150", description: "A family of mice living in a quiet suburban home are uprooted by a group of streetwise cats."},
+        {title: "Home", image_url: "https://via.placeholder.com/150", description: "A family of mice living in a quiet suburban home are uprooted by a group of streetwise cats."},
+        {title: "Home", image_url: "https://via.placeholder.com/150", description: "A family of mice living in a quiet suburban home are uprooted by a group of streetwise cats."},
+      ]
+    };
+  },
+  methods: {
+    handleSearch(query) {
+      return `<div>Searching for: ${query}</div>`;
+      
+    }
+  }
+};
+</script>
+
 <template>
+  <div>
+    <Searchbar @search="handleSearch"/>
+  </div>
+
   <ul class="flex flex-col gap-3 md:gap-5 p-2 lg:grid lg:grid-cols-2">
     <li v-if="filmes.length === 0">
       <h2>No movies found {{ " :(" }}</h2>
