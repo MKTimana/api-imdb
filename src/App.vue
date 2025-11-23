2f4de5d458553db0cfb5d21c0cba12404b4e2bb1
Hamzah Braimo <hamzah.brm@gmail.com>
2025-11-23 23:43:54 +0200

    Added a search bar (search input + search button)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script>
 import Card from './components/Card.vue'
+import Searchbar from './components/Searchbar.vue'
 export default {
   components: {
-    Card
+    Card, Searchbar
   },
   data() {
     return {
@@ -12,11 +13,21 @@ export default {
         {title: "Home", image_url: "https://via.placeholder.com/150", description: "A family of mice living in a quiet suburban home are uprooted by a group of streetwise cats."},
       ]
     };
+  },
+  methods: {
+    handleSearch(query) {
+      return `<div>Searching for: ${query}</div>`;
+      
+    }
   }
 };
 </script>
 
 <template>
+  <div>
+    <Searchbar @search="handleSearch"/>
+  </div>
+
   <ul class="flex flex-col gap-3 md:gap-5 p-2 lg:grid lg:grid-cols-2">
     <li v-for="(movie, index) in movies" :key="index">
       <Card :movie="movie" />
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,53 @@
+<script>
+  export default {
+    name: "Seachbar",
+
+    data() {
+      return {
+        query: ""
+      };
+    },
+
+    methods: {
+      search(){
+        this.$emit("search", this.query);
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="searchbar">
+    <input
+      type="text"
+      v-model="query"
+      placeholder="Search for a movie, tv show, documentary, ..."
+      @keyup.enter="search"
+    />
+
+    <button @click="search" class="search-btn">Search</button>
+  </div>
+</template>
+
+<style>
+  .searchbar {
+    display: flex;
+    gap: 8px;
+  }
+
+  input {
+    flex: 1;
+    padding: 6px;
+  }
+
+  .search-btn{
+    border: none;
+    border-radius: 5px;
+    padding: .8em 1.5em;
+    background-color: black;
+    font-family: Arial;
+    color: white;
+    
+    cursor: pointer;
+  }
+</style>
