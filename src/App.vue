57e99185a3bba8390fc978d4137343c691f5ef52
SoG-WRLD <gilsongvicente.999@gmail.com>
2025-11-24 00:39:13 +0200

    fixed connection to api

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <ul class="flex flex-col gap-3 md:gap-5 p-2 lg:grid lg:grid-cols-2">
-    <li v-for="(movie, index) in movies" :key="index">
+    <li v-if="filmes.length === 0">
+      <h2>No movies found {{ " :(" }}</h2>
+    </li>
+    <li v-else v-for="(movie, index) in filmes" :key="index">
       <Card :movie="movie" />
     </li>
   </ul>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import { getPopularMovies } from './api/service';
+import { ref, onMounted } from "vue";
+import { getPopularMovies } from "./api/service";
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'IMDB',
+  name: "IMDB",
+  components: { Card },
   setup() {
-    const filmes = ref([])
+    const filmes = ref([]);
 
     onMounted(async () => {
-      const data = await getPopularMovies()
-      filmes.value = data
-    })
-    return { filmes }
-  }
-}
+      const data = await getPopularMovies();
 
+      filmes.value = data;
+    });
+    return { filmes };
+  },
+};
 </script>
 
-<style>
-#app {
-  text-align: center;
-}
-
-.movie {
-  display: inline-block;
-  margin: 10px;
-  text-align: center;
-}
-
-.movie img {
-  width: 200px;
-  height: auto;
-}
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,6 +5,7 @@ defineProps({
     required: true,
   },
 });
+
 </script>
 
 <template>
@@ -12,22 +13,27 @@ defineProps({
     class="w-full md:w-2/3 lg:w-full dark:bg-dark-200/30 flex gap-3 text-dark-300 dark:text-surface-100 rounded-xl p-4 shadow-md shadow-black/50"
   >
     <img
-      :src="movie.image_url"
+      :src="`https://api.themoviedb.org/3${movie.poster_path}`"
       :alt="movie.title"
-      class="flex min-w-32 max-w-48 min-h-44 max-h-56 border shrink-0"
+      class="flex min-w-32 max-w-40 min-h-44 max-h-56 border shrink-0 text-sm"
     />
     <span class="flex flex-col gap-1 sm:gap-2.5 justify-evenly">
       <h2 class="text-xl font-semibold w-full border-b-2 border-accent-300">
         {{ movie.title }}
       </h2>
-      <p
-        class="text-justify text-clip text-sm sm:text-base md:text-lg max-h-32 line-clamp-4"
+      <ul
+        class="text-justify text-xs sm:text-sm md:text-base max-h-32 line-clamp-4"
       >
-        {{ movie.description }} Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Dicta, inventore impedit nulla ipsa officia incidunt
-        at numquam laudantium sit, laborum culpa quia, molestiae minus amet
-        exercitationem vitae nostrum quo! Rem!
-      </p>
+      <li>
+        Release date: {{ (movie.release_date) }}
+      </li>
+      <li>
+        Popularity: {{ Math.floor(movie.popularity) }}
+      </li>
+      <li>
+        Rating: {{ (movie.vote_average) }}
+      </li>
+    </ul>
       <button
         class="outline-0 transition-smooth self-center w-full rounded-lg shadow-sm shadow-black/30 cursor-pointer hover:scale-95 px-2 py-0.5 bg-linear-to-t from-accent-300 to-accent-200 sm:max-w-1/2"
       >
